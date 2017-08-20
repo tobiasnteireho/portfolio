1501a9e4fa3c386a663e4294f00ce0707fad2bcc
Tobias Nteireho <tobiasnte@gmail.com>
2017-08-20 16:34:33 -0500

    Create logic.js

---
+++ Javascript/FreeCodeCamp/projects/intermediate/quotes/logic.js
@@ -0,0 +1,26 @@
+body {
+	font-family: Lobster;
+	background-color: #FFEEAA;
+}
+
+#thequote {
+	width: 500px;
+	height: 300px;
+	background-color: #DEEFAA;
+	font-size: 2em;
+}
+
+#theAuthor{
+	width: 500px;
+	height: 50px;
+	background-color: #DDFFAA;
+	text-align: right;
+	font-size: 1.5em;
+	color: #A3A3A3;
+}
+
+#.parent {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
