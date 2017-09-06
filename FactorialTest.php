6857e6fdbc193ac1a514d7b56a47749576b0e78a
Tobias Nteireho <tobiasnte@gmail.com>
2017-09-06 10:34:45 -0500

    Add files via upload

---
+++ FactorialTest.php
@@ -0,0 +1,43 @@
+<?php 
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers Factorialize
+*/
+
+final class FactorialTest extends TestCase
+{
+	public function testCase1(): void
+	{
+		$this->assertEquals(120, Ending::factorialize(5)));
+	}
+	
+	public function testCase2(): void
+	{
+		$this->assertEquals(3628800, Ending::factorialize(10);
+	}
+	
+	public function testCase3(): void
+	{
+		$this->assertEquals(2432902008176640000, Ending::factorialize(20));
+	}
+	
+	public function testCase4(): void
+	{
+		$this->assertEquals(1, factorialize(0));
+	}
+
+
+}
+
+
+/*
+
+factorialize(5) should return a number.
+factorialize(5) should return 120.
+factorialize(10) should return 3628800.
+factorialize(20) should return 2432902008176640000.
+factorialize(0) should return 1.
+*/
